fea3727c602d143f496a26816c2e3e355cc7fb83
Vivien <vivien.goepp@gmail.com>
2021-05-30 17:57:02 +0200

    rm simu.R

--- R/simu.R
+++
@@ -1,9 +0,0 @@
-# old_zones <- list(1, 20, 381, 400)
-# while (length(setdiff(unlist(zones), seq(1, p))) > 0) {
-#
-#   zones <- adjacent_vertices(graph, old_zones) %>%
-#     mapply(union, ., old_zones, SIMPLIFY = FALSE)
-#   zones
-#   old_zones <- zones
-# }
-# zones
